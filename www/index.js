fc6d7371da527181c9146084cf1048883916687a
feruzboboqulov02 <feruzboboqulov002@gmail.com>
2025-07-09 10:31:18 +0500

    connect socket

--- www/index.js
+++ www/index.js
@@ -3,12 +3,25 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-
-app.listen(3000, () => {
-    console.log('Server running at http://localhost:3000/');
-});
-
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+const users = [];
+const connections = [];
+
+io.sockets.on('connection', function (socket) {
+    console.log("Successfully Connected to Socket.IO");
+    connections.push(socket);
+    console.log('Connected: %s sockets connected', connections.length);
+
+    // Disconnect
+    socket.on('disconnect', function (data) {
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnected: %s sockets connected', connections.length);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000/');
+});
